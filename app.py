3165d86cb74b256795d2052c46cbdb1e4ee24735
Hibah Abdul Razak <hibahar321@gmail.com>
2025-11-14 22:21:51 +0530

    added GET and POST methods

--- app.py
+++ app.py
@@ -1,17 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) 
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    # return "<p>Hello, World!!!!!!!!!</p>"
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
 
-@app.route("/products")
+        # IMPORTANT: redirect after POST
+        return redirect(url_for('hello_world'))
+
+    # This only runs on GET
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route("/show")
 def products():
+    allTodo = Todo.query.all()
+    print(allTodo)
     return "<p>This is a products page</p>"
+
+@app.route("/update")
+def update():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return "<p>This is a products page</p>"
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
